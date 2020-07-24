07db2247ffdbbca895bae9e3ce7a31109ad1c990
Jason Farquhar <jadref@gmail.com>
2020-07-24 11:24:34 +0200

    again

--- bin/debug_quickstart.sh
+++ bin/debug_quickstart.sh
@@ -3,11 +3,11 @@ cd `dirname ${BASH_SOURCE[0]}`
 buffdir=`dirname $0`
 
 echo Starting the utopia-hub
-startUtopiaHub.sh &
+./startUtopiaHub.sh &
 utopia2ftpid=$!
 echo utopia2ftpid=$utopia2ftpid
 sleep 5
 
 #resources/eeg/startJavaSignalproxy.sh > /dev/null 
-startUtopiaClient.sh data 200
+./startUtopiaClient.sh data 200
 
