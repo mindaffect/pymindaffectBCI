ca61667dd89a193a97908f01a4c40e2d4cf0ec91
Jason Farquhar <jadref@gmail.com>
2020-07-24 11:18:47 +0200

    fix the scripts

--- bin/debug_quickstart.sh
+++ bin/debug_quickstart.sh
@@ -3,19 +3,11 @@ cd `dirname ${BASH_SOURCE[0]}`
 buffdir=`dirname $0`
 
 echo Starting the utopia-hub
-java/messagelib/startUtopiaHub.sh &
+startUtopiaHub.sh &
 utopia2ftpid=$!
 echo utopia2ftpid=$utopia2ftpid
 sleep 5
 
 #resources/eeg/startJavaSignalproxy.sh > /dev/null 
-java/messagelib/testclient.sh data
-dataacqpid=$!
-echo dataacqpid=$dataacqpid
+startUtopiaClient.sh data 200
 
-echo Starting the event viewer
-#bash resources/eeg/startJavaEventViewer.sh
-#bash matlab/utopia/startRecogniser.sh 
-kill $utopia2ftpid
-kill $dataacqpid
-#kill $bufferpid
