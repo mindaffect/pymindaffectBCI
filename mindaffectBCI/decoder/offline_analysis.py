a12fda24351c38b666c46089af06cec3126a39ba
Jason Farquhar <jadref@gmail.com>
2020-10-05 16:34:35 +0200

    updated on-line time-stamp filtering function.  timestamp checking function.
    Former-commit-id: 20788484ad10d019110242e9713df4647b519603

--- mindaffectBCI/decoder/offline_analysis.py
+++ mindaffectBCI/decoder/offline_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 from mindaffectBCI.decoder.analyse_datasets import debug_test_dataset, analyse_dataset, analyse_datasets
 from mindaffectBCI.decoder.offline.load_mindaffectBCI  import load_mindaffectBCI
+from mindaffectBCI.decoder.timestamp_check import timestampPlot
 import matplotlib.pyplot as plt
 
 savefile = '~/Downloads/mindaffectBCI_200701_1617[1].txt'
@@ -9,27 +10,35 @@ savefile = '~/Downloads/mindaffectBCI_200701_1624[1].txt'
 
 savefile = '~/Downloads/mindaffectBCI_200701_2025_khash.txt'
 
-# get raw
-X, Y, coords = load_mindaffectBCI(savefile, stopband=None, ofs=200)
+savefile = '~/Desktop/mark/mindaffectBCI*ganglion*1411*.txt'
 
-# get normally pre-processed
-X, Y, coords = load_mindaffectBCI(savefile, stopband=((0,5),(25,-1)), order=6, ofs=200)
+import glob
+import os
+files = glob.glob(os.path.expanduser(savefile)); 
+#os.path.join(os.path.dirname(os.path.abspath(__file__)),fileregexp)) # * means all if need specific format then *.csv
+savefile = max(files, key=os.path.getctime)
+
+# load
+X, Y, coords = load_mindaffectBCI(savefile, stopband=((45,65),(5.5,25,'bandpass')), order=6, ftype='butter', ofs=100)
 # output is: X=eeg, Y=stimulus, coords=meta-info about dimensions of X and Y
 print("EEG: X({}){} @{}Hz".format([c['name'] for c in coords],X.shape,coords[1]['fs']))
 print("STIMULUS: Y({}){}".format([c['name'] for c in coords[:1]]+['output'],Y.shape))
 
 plt.close('all')
-debug_test_dataset(X, Y, coords, tau_ms=350, evtlabs=('re','fe'), rank=1, model='cca')
+debug_test_dataset(X, Y, coords, tau_ms=450, evtlabs=('re','fe'), rank=1, model='cca')
 
 
-# check the electrode qualities computation
-ppfn= butterfilt_and_downsample(order=6, stopband='butter_stopband((0, 5), (25, -1))_fs200.pk', fs_out=100)
-X=X.reshape((-1,X.shape[-1]))
-ppfn.fit(X[:10,:],fs=200)
-Xp =  ppfn.transform(X)
-plt.clf();plt.subplot(211);plt.plot(X);plt.subplot(212);plt.plot(Xp);
+# do a time-stamp check.
+timestampPlot(savefile)
 
-sigq=testElectrodeQualities(X,fs=200)
-plt.clf();plt.plot(sigq)
+# # check the electrode qualities computation
+# ppfn= butterfilt_and_downsample(order=6, stopband='butter_stopband((0, 5), (25, -1))_fs200.pk', fs_out=100)
+# X=X.reshape((-1,X.shape[-1]))
+# ppfn.fit(X[:10,:],fs=200)
+# Xp =  ppfn.transform(X)
+# plt.clf();plt.subplot(211);plt.plot(X);plt.subplot(212);plt.plot(Xp);
+
+# sigq=testElectrodeQualities(X,fs=200)
+# plt.clf();plt.plot(sigq)
 
 
