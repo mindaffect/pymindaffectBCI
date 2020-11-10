p-val computation fixes and sensible defaults

mindaffectBCI/decoder/decodingCurveSupervised.py
@@ -29,6 +29,8 @@ def decodingCurveSupervised(Fy,objIDs=None,nInt=(30,25),**kwargs):
     if Fy is None:
         return 1, 1, None, None, None, None, -1, 1
 
+    #print("kwargs={}".format(kwargs))
+
     # remove trials with no-true-label info
     keep = np.any(Fy[..., objIDs == 0], (-2, -1) if Fy.ndim<=3 else (0,-2,-1)) # [ nTrl ]
     if not np.all(keep):
@@ -193,7 +195,12 @@ def plot_decoding_curve(integerationLengths, aveProbErr, *args):
         if len(args)>=7-2:
             # plot the trialwise estimates, when is single subject
             Yerr = args[5-2] #(nTrl,nInt), flag if was right or not
-            Perr = args[6-2].copy() #(nTrl,nInt)
+            oPerr = args[6-2] #(nTrl,nInt)
+            keep = np.any(oPerr<1,axis=-1) #(nTrl)
+            Yerr=Yerr[keep,:]
+            oPerr=oPerr[keep,:]
+
+            Perr=oPerr.copy()
             plt.plot(integerationLengths.T,Perr.T,color='.95') # line per trial
             Perr[Yerr<0]=np.NaN
             Perr[Yerr==True]=np.NaN # disable points where it was in error
@@ -201,7 +208,7 @@ def plot_decoding_curve(integerationLengths, aveProbErr, *args):
             plt.plot(integerationLengths.T,Perr[0,:].T,'.',markerfacecolor=(0,1,0,.2),markeredgecolor=(0,1,0,.2),label='Perr(correct)')
             plt.plot(integerationLengths.T,Perr.T,'.',markerfacecolor=(0,1,0,.2),markeredgecolor=(0,1,0,.2))
             # est when incorrect..
-            Perr = args[6-2].copy() #(nTrl,nInt)
+            Perr = oPerr.copy() #(nTrl,nInt)
             Perr[Yerr<0]=np.NaN
             Perr[Yerr==False]=np.NaN # disable points where it was in error, or not available
             plt.plot(integerationLengths.T,Perr[0,:].T,'.', markerfacecolor=(1,.0,.0,.2), markeredgecolor=(1,.0,.0,.2),label='Perr(incorrect)')

mindaffectBCI/decoder/decodingSupervised.py
@@ -2,17 +2,11 @@ import numpy as np
 from mindaffectBCI.decoder.normalizeOutputScores import normalizeOutputScores
 from mindaffectBCI.decoder.zscore2Ptgt_softmax import zscore2Ptgt_softmax
 #@function
-def decodingSupervised(Fy, softmaxscale=3.5, badFyThresh=4,
-                       centFy=True, detrendFy=False, 
-                       nEpochCorrection=100,
-                       minDecisLen=0, maxDecisLen=0,
-                       bwdAccumulate=False, normSum=True,
-                       marginalizemodels=True, 
+def decodingSupervised(Fy, softmaxscale=3.5, marginalizemodels=True, 
                        marginalizedecis=False,
                        prior=None,
                        nocontrolamplitude=None,
-                       priorsigma=(-1,120),
-                       tiebreaking_noise=1e-3):
+                       tiebreaking_noise=1e-3, **kwargs):
   """    true-target estimator and error-probility estimator for each trial
 
    Args:
@@ -54,13 +48,7 @@ def decodingSupervised(Fy, softmaxscale=3.5, badFyThresh=4,
   # get the info on which outputs are zero in each trial
   validTgt = np.any(Fy != 0, axis=-2) # valid if non-zero for any epoch..# (nModel,nTrl,nY)  
   # normalize the raw scores for each model to have nice distribution
-  ssFy,varsFy,decisIdx,nEp,nY=normalizeOutputScores(Fy, validTgt=validTgt,
-                                              badFyThresh=badFyThresh, 
-                                              centFy=centFy, detrendFy=detrendFy,
-                                              nEpochCorrection=nEpochCorrection,
-                                              minDecisLen=minDecisLen, maxDecisLen=maxDecisLen,
-                                              bwdAccumulate=bwdAccumulate, normSum=normSum,
-                                              priorsigma=priorsigma)
+  ssFy,varsFy,decisIdx,nEp,nY=normalizeOutputScores(Fy, validTgt=validTgt, **kwargs)
 
   if nocontrolamplitude is not None:
     raise NotImplementedError('no-control signal not yet implemented correctly')
@@ -99,10 +87,7 @@ def decodingSupervised(Fy, softmaxscale=3.5, badFyThresh=4,
   return Yest, Perr, Ptgt, decisMdl, decisEp
 
 
-def decodingSupervised_streamed(Fy,softmaxscale=3,nocontrolamplitude=None,badFyThresh=6,
-                                centFy=True,nEpochCorrection=15,
-                                minDecisLen=0,maxDecisLen=0,
-                                bwdAccumulate=False, filtLen=15,
+def decodingSupervised_streamed(Fy,softmaxscale=3,nocontrolamplitude=None,
                                 marginalizemodels=False):
     '''
     true-target estimator and error-probility estimator for each trial

mindaffectBCI/decoder/normalizeOutputScores.py
@@ -1,11 +1,12 @@
+#from mindaffectBCI.decoder.zscore2Ptgt_softmax import softmax
 import numpy as np
 
 def normalizeOutputScores(Fy, validTgt=None, badFyThresh=4,
-                          centFy=True, detrendFy=False, 
+                          normSum=False, centFy=False, detrendFy=False, 
                           nEpochCorrection=0,
                           minDecisLen=0, maxDecisLen=0,
                           bwdAccumulate=False,
-                          priorsigma=None, normSum=False, marginalizemodels=True):
+                          priorsigma=None, marginalizemodels=True):
     '''
     normalize the raw output scores to feed into the Perr computation
 
@@ -34,6 +35,10 @@ def normalizeOutputScores(Fy, validTgt=None, badFyThresh=4,
     if Fy is None or Fy.size == 0:
         ssFy = np.zeros(Fy.shape[:-2]+(1,Fy.shape[-1]))
         return ssFy, None, 0, None, None
+
+    # print('args={}'.format(dict(marginalizemodels=marginalizemodels,normSum=normSum,
+    #                         detrendFy=detrendFy,centFy=centFy,nEpochCorrection=nEpochCorrection,
+    #                         priorsigma=priorsigma)))
 
     # compress out the model dimension
     # Fyshape = Fy.shape # (nM,nTrl,nEp,nY)
@@ -251,7 +256,7 @@ def estimate_Fy_noise_variance(Fy, decisIdx=None, centFy=True, detrendFy=False, 
     nvar2csFy = var2csFy / np.maximum(1, N, dtype=scFy.dtype) #np.arange(1,var2csFy.shape[-1]+1) # ave var per-time-step
 
     # compute the average of the estimated slopes for each integeration length
-    muvar2csFy = np.cumsum(nvar2csFy, -1) / np.maximum(1,np.cumsum(N>0,-1))[np.newaxis,:,:] #arange(1,nvar2csFy.shape[-1]+1)[np.newaxis,np.newaxis,:] #np.maximum(1, N, dtype=scFy.dtype) # ave per-stime-stamp vars before each time-point
+    muvar2csFy = np.cumsum(nvar2csFy, -1) / np.maximum(1, np.cumsum(N>0,-1))  # ave per-stime-stamp vars before each time-point
     
     # return the ave-cumsum-slope-of-variance at each decision length
     sigma2 = muvar2csFy[...,decisIdx] # (nTr,nDecis)

mindaffectBCI/decoder/zscore2Ptgt_softmax.py
@@ -1,3 +1,4 @@
+from mindaffectBCI.decoder.normalizeOutputScores import mktestFy
 import numpy as np
 
 def zscore2Ptgt_softmax(f, softmaxscale:float=2, prior:np.ndarray=None, validTgt=None, marginalizemodels:bool=True, marginalizedecis:bool=False, peroutputmodel:bool=True):
@@ -183,11 +184,7 @@ def calibrate_softmaxscale(f, validTgt=None, scales=(.01,.02,.05,.1,.2,.3,.4,.5,
     print("softmaxscale={}".format(softmaxscale))
     return softmaxscale
 
-
-def testcase(nY=10, nM=4, nEp=340, nTrl=500, sigstr=.4, normSum=False, marginalizemodels=True, marginalizedecis=False, nEpochCorrection=100, priorweight=0, startup_lag=.1):
-    import numpy as np
-    print("{}".format(locals()))
-
+def mkTestFy(nY,nM,nEp,nTrl,sigstr,startup_lag):
     np.random.seed(0)
     noise = np.random.standard_normal((nM,nTrl,nEp,nY))
     noise = noise - np.mean(noise.ravel())
@@ -203,6 +200,12 @@ def testcase(nY=10, nM=4, nEp=340, nTrl=500, sigstr=.4, normSum=False, marginali
     # add the signal
     Fy[0, :, :, 0] = Fy[0, :, :, 0] + sigamp
     #print("Fy={}".format(Fy))
+
+    return Fy, noise, sigamp
+
+def visPtgt(Fy, normSum=True, centFy=True, detrendFy=True, marginalizemodels=True, marginalizedecis=False, nEpochCorrection=20, priorweight=1e2):
+    import numpy as np
+    #print("{}".format(locals()))
     
     sFy=np.cumsum(Fy,-2)
     from mindaffectBCI.decoder.normalizeOutputScores import normalizeOutputScores, estimate_Fy_noise_variance
@@ -211,18 +214,25 @@ def testcase(nY=10, nM=4, nEp=340, nTrl=500, sigstr=.4, normSum=False, marginali
     sigma0 = np.nanmedian(sigma0)  # ave
     print('Sigma0 = {}'.format(sigma0))
 
-    ssFy,scale_sFy,N,_,_=normalizeOutputScores(Fy,minDecisLen=-1, nEpochCorrection=nEpochCorrection, normSum=normSum, marginalizemodels=marginalizemodels, priorsigma=(sigma0,priorweight))
+    ssFy,scale_sFy,N,_,_=normalizeOutputScores(Fy,minDecisLen=-1, nEpochCorrection=nEpochCorrection, 
+                                normSum=normSum, detrendFy=detrendFy, centFy=centFy, 
+                                marginalizemodels=marginalizemodels, priorsigma=(sigma0,priorweight))
     softmaxscale = calibrate_softmaxscale(ssFy,marginalizemodels=marginalizemodels)
     #print('ssFy={}'.format(ssFy.shape))
     from mindaffectBCI.decoder.zscore2Ptgt_softmax import zscore2Ptgt_softmax, softmax
-    smax = softmax(ssFy*sigstr,axis=(-4,-1))
+    smax = softmax(ssFy*softmaxscale,axis=((-4,-1) if ssFy.ndim>3 else -1))
     #print("{}".format(smax.shape))
     Ptgt=zscore2Ptgt_softmax(ssFy, marginalizemodels=marginalizemodels, marginalizedecis=marginalizedecis, softmaxscale=softmaxscale) # (nTrl,nEp,nY)
     #print("Ptgt={}".format(Ptgt.shape))
     import matplotlib.pyplot as plt
     plt.clf()
     tri=1
+    nM=Fy.shape[-4] if Fy.ndim>3 else 1
     for mi in range(nM):
+        sFyi  = sFy[mi,tri,:,:] if sFy.ndim>3 else sFy[tri,:,:]
+        ssFyi = ssFy[mi,tri,:,:] if ssFy.ndim>3 else ssFy[tri,:,:]
+        smaxi = smaxi[mi,tri,:,:] if smax.ndim>3 else smax[tri,:,:]
+
         if mi==0 :
             a = plt.subplot(4,nM,0*nM+mi+1)
             xax=a
@@ -230,7 +240,7 @@ def testcase(nY=10, nM=4, nEp=340, nTrl=500, sigstr=.4, normSum=False, marginali
         else:
             a = plt.subplot(4,nM,0*nM+mi+1,sharex=xax, sharey=yax0)
         plt.cla()
-        plt.plot(sFy[mi,tri,:,:])
+        plt.plot(sFyi)
         plt.plot(scale_sFy[tri,:],'k')
         if mi==0 : plt.ylabel('sFy')
         plt.ylim((np.min(sFy.ravel()),np.max(sFy.ravel())))
@@ -242,7 +252,7 @@ def testcase(nY=10, nM=4, nEp=340, nTrl=500, sigstr=.4, normSum=False, marginali
         else:
             a=plt.subplot(4,nM,1*nM+mi+1, sharex=xax, sharey=yax1)
         plt.cla()
-        plt.plot(ssFy[mi,tri,:,:])
+        plt.plot(ssFyi)
         plt.ylim((np.min(ssFy.ravel()),np.max(ssFy.ravel())))
         if mi==0 : plt.ylabel('ssFy')
         plt.grid()
@@ -253,8 +263,8 @@ def testcase(nY=10, nM=4, nEp=340, nTrl=500, sigstr=.4, normSum=False, marginali
         else:
             a=plt.subplot(4,nM,2*nM+mi+1, sharex=xax, sharey=yax2)
         plt.cla()
-        plt.plot(smax[mi,tri,:,:])
-        plt.ylim((np.min(smax[:,tri,:,:].ravel()),np.max(smax[:,tri,:,:].ravel())))
+        plt.plot(smaxi)
+        plt.ylim((np.min(smax.ravel()),np.max(smax.ravel())))
         if mi==0: plt.ylabel('softmax')
         plt.grid()
         
@@ -269,14 +279,32 @@ def testcase(nY=10, nM=4, nEp=340, nTrl=500, sigstr=.4, normSum=False, marginali
     from mindaffectBCI.decoder.decodingCurveSupervised import decodingCurveSupervised, plot_decoding_curve
     #ssFy,scale_sFy,N,_,_=normalizeOutputScores(Fy,minDecisLen=-1, nEpochCorrection=nEpochCorrection, normSum=normSum, marginalizemodels=marginalizemodels)
     #softmaxscale = calibrate_softmaxscale(ssFy,marginalizemodels=marginalizemodels)
-    (dc) = decodingCurveSupervised(Fy,nInt=(100,50),marginalizemodels=marginalizemodels,normSum=normSum,nEpochCorrection=nEpochCorrection,softmaxscale=softmaxscale,priorsigma=(sigma0,priorweight))
+    (dc) = decodingCurveSupervised(Fy,nInt=(100,50),
+                    marginalizemodels=marginalizemodels,
+                    normSum=normSum, detrendFy=detrendFy, centFy=centFy, nEpochCorrection=nEpochCorrection,
+                    softmaxscale=softmaxscale,priorsigma=(sigma0,priorweight))
     plt.figure()
     plot_decoding_curve(*dc)
     plt.show()
 
+def testcase(nY=10, nM=4, nEp=340, nTrl=500, sigstr=.4, normSum=True, centFy=True, detrendFy=True, marginalizemodels=True, marginalizedecis=False, nEpochCorrection=20, priorweight=1e2, startup_lag=.1):
+
+    # mk the test dataset
+    Fy, noise, sigamp = mkTestFy(nY,nM,nEp,nTrl,sigstr,startup_lag)
+
+    # visualize it
+    visPtgt(Fy,normSum,centFy,detrendFy,marginalizemodels,marginalizedecis,nEpochCorrection,priorweight)
 
 
 if __name__=="__main__":
-    testcase(nY=10, nM=4, nEp=340, nTrl=200, sigstr=.4, startup_lag=.1, 
-            normSum=False, marginalizemodels=True, marginalizedecis=False, 
-            nEpochCorrection=0, priorweight=0)
+    if False:
+        Fy, noise, sigamp = mkTestFy(nY=10,nM=4,nEp=640,nTrl=200,sigstr=.15,startup_lag=.05)
+    else:
+        import pickle
+        stopping=pickle.load(open('stopping.pk','rb'))
+        Fy = stopping['Fy']
+        keep = np.any(Fy>0,axis=(-2,-1) if Fy.ndim<4 else (-4,-2,-1))
+        Fy=Fy[...,keep,:,:]
+
+    visPtgt(Fy,normSum=False,centFy=False,detrendFy=False,
+            marginalizemodels=True,marginalizedecis=False,nEpochCorrection=50,priorweight=200)