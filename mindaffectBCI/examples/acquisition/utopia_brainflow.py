4f1ad8dd30d8f042bb353cb7bb6563765b6374f9
khashdroid <60778315+khashdroid@users.noreply.github.com>
2020-12-03 18:25:04 +0100

    add copyright

--- mindaffectBCI/examples/acquisition/utopia_brainflow.py
+++ mindaffectBCI/examples/acquisition/utopia_brainflow.py
@@ -1,3 +1,29 @@
+# Copyright (c) 2020 MindAffect B.V. 
+#  Author: Khashayar Sharif <khash@mindaffect.nl>
+#  Build on top of brainflow and MindAffect's original brainflow based EEG
+#  driver written by Jason Farquhar. 
+#  Extra features added by this code:
+#  1.local openbci cyton timestamps are used instead of host timestamps.
+#  2.The user has the option of choosing/changinh the sampling frequency of the eeg frontened.
+#  3.The user has the option to set the board to bipolar mode to perform a triggercheck.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import argparse
 from brainflow.board_shim import BoardShim, BrainFlowInputParams
 from mindaffectBCI import utopiaclient 
