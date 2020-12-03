6b56991a9c35010db566526b7c2cc71a929c454c
Jason Farquhar <jadref@gmail.com>
2020-12-03 20:52:15 +0100

    15hz min ssvep freq

--- mindaffectBCI/stimseq.py
+++ mindaffectBCI/stimseq.py
@@ -287,7 +287,7 @@ def mkRowCol(width=5,height=5, repeats=10):
     return StimSeq(None,array.tolist(),None)
 
 
-def mkFreqTag(period_phase=((3,0),(4,0),(5,0),(6,0),(7,0),(3,1),(4,1),(5,1),(6,1),(7,1),(3,2),(4,2),(5,2),(6,2),(7,2),(4,3),(5,3),(6,3),(7,3),(5,4),(6,4),(7,4),(6,5),(7,5),(7,6)),nEvent=120, isbinary=True):
+def mkFreqTag(period_phase=((4,0),(5,0),(6,0),(7,0),(8,0),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(3,2),(4,2),(5,2),(6,2),(7,2),(8,2),(4,3),(5,3),(6,3),(7,3),(8,3),(5,4),(6,4),(7,4),(8,4),(6,5),(7,5),(8,5),(7,6),(8,6),(8,7)),nEvent=120, isbinary=True):
     """Generate a frequency tagging stimulus sequence
 
     Args:
