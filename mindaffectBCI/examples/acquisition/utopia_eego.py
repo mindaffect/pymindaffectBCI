0d40c64f4602dd1fdb3131d8ae1314388bb6a31c
Jason Farquhar <jadref@gmail.com>
2020-12-03 11:47:10 +0100

    update eego license info

--- mindaffectBCI/examples/acquisition/utopia_eego.py
+++ mindaffectBCI/examples/acquisition/utopia_eego.py
@@ -5,9 +5,12 @@ def run(**kwargs):
     """startup the acquisation driver for a BrainProducts based amplifiers. 
 
     Note: This driver includes software developed by the eemagine Medical Imaging Solutions GmbH, 
-    and is based on the AntNeuro SDK -- and limited by the license 
-    limitations of that SDK as described here <https://github.com/mindaffect/pymindaffectBCI/examples/acquisation/UDO-SM-0124 rev7.1 eego amplifier software interface SDK user manual EN 2018-11-12.pdf>.
+    and is built using the AntNeuro SDK (version : 1.3.19 released 11-2018) -- and limited by the license 
+    limitations of that SDK as described here on Page 6 <https://github.com/mindaffect/pymindaffectBCI/examples/acquisation/UDO-SM-0124 rev7.1 eego amplifier software interface SDK user manual EN 2018-11-12.pdf> 
+    (Note: SDK users should *not* use this document as the SDK has since been updated, but refer to the documentation provided with their SDK.  This document is provided only for the license information on Page6)
     In summary, this driver is provided without WARRANTY, for non-commercial use.
+
+    Further, eegmagine Medical Imaging Solutions 
 
     Returns:
         subprocess: subprocess object for communicating with the started amplifier driver.
