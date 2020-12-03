9ee9b8df67235647a9f943a1dd482375b2d38f84
khashdroid <60778315+khashdroid@users.noreply.github.com>
2020-12-03 18:27:43 +0100

    Update CytonWithTimeStamps.py

--- mindaffectBCI/examples/acquisition/CytonWithTimeStamps.py
+++ mindaffectBCI/examples/acquisition/CytonWithTimeStamps.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2019 MindAffect B.V. 
 #  Author: Khashayar Sharif <khash@mindaffect.nl>
 #  Build on top of brainflow and MindAffect's original brainflow based EEG
-#  driver
+#  driver written by Jason Farquhar
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
