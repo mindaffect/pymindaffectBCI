fix the virt-out in scale computation

mindaffectBCI/decoder/decodingCurveSupervised.py
@@ -282,12 +282,19 @@ def testcase():
     """    
     import numpy as np
     import matplotlib.pyplot as plt
-    Fy=np.random.standard_normal((2,10,100,50))
-    Fy[0,:,:,0]=Fy[0,:,:,0] + 0.3
-    from decodingCurveSupervised import decodingCurveSupervised
-    (dc)=decodingCurveSupervised(Fy)
+    from mindaffectBCI.decoder.normalizeOutputScores import mktestFy,  normalizeOutputScores
+    Fy, nEp = mktestFy(sigstr=.5,nM=1,nY=10,nTrl=10,startupNoisefrac=25) #(nM, nTrl, nEp, nY)
+    #Fy = Fy[0,...] # (nTrl,nEp,nY)
+    # Introduce temporal and spatial sparsity like real data
+    Fy = Fy * (np.random.standard_normal((1,Fy.shape[-2],Fy.shape[-1]))>0).astype(np.float)
+    Fy[...,:50,:] = 0 # block zero at start
+
+    from mindaffectBCI.decoder.decodingCurveSupervised import decodingCurveSupervised
+    (dc)=decodingCurveSupervised(Fy,nInt=(25,25),nvirt_out=-12,softmaxscale=2)
     plot_decoding_curve(*dc)
-    plt.show(block=False)
+    plt.show(block=True)
+
+    quit()
 
     sFy = np.cumsum(Fy,-2)
     Yi = np.argmax(sFy,-1)

mindaffectBCI/decoder/normalizeOutputScores.py
@@ -409,35 +409,35 @@ def plot_normalizedScores(Fy, ssFy, scale_sFy, decisIdx):
 
 
 #@function
-def mktestFy(nY=10, nM=1, nEp=360, nTrl=100, sigstr=.5, startupNoisefrac=.25, offsetstr=10, trlenfrac=.25):
+def mktestFy(nY=10, nM=1, nEp=360, nTrl=100, sigstr=.5, startupNoisefrac=.25, offsetstr=0, trlenfrac=.25):
     import numpy as np
     noise = np.random.standard_normal((nM, nTrl, nEp, nY))
-    noise = noise - np.mean(noise.ravel())
-    noise = noise / np.std(noise.ravel())
+    noise = noise - np.mean(noise,axis=(-1,-2),keepdims=True)
+    noise = noise / np.std(noise,axis=(-1,-2),keepdims=True)
     # add an offset to the scores...
     offset = offsetstr*np.random.standard_normal((noise.shape[-2])) # (nEp)
     noise = noise+offset[:, np.newaxis] # (nM, nTrl, nEp, nY) [ nYxnEpxnTrlxnM ]
 
     sigamp = sigstr*np.ones((noise.shape[-2])) # (nEp)
 
+    # make variable length trials,  and per-trial offset
+    nEp = np.zeros((noise.shape[-3],), dtype=int)
+    for ti in range(noise.shape[-3]):
+        # trial offset
+        mu = np.random.standard_normal()
+        noise[:,  ti, :, :] = noise[:, ti, :, :] + mu
+        # trial end time
+        nEp[ti] = noise.shape[-2]*(np.random.uniform()*trlenfrac + (1-trlenfrac))
+        noise[:, ti, nEp[ti]:, :] = 0
+
+    # no signal at the start of the trial
+    startupNoise_samp = int(noise.shape[1]*startupNoisefrac) if startupNoisefrac<1 else startupNoisefrac
+    sigamp[:startupNoise_samp] = 0
+    noise[:,:,:startupNoise_samp,:]=0
+
     # measure is sig + noise
     Fy = noise
     Fy[0, :, :, 0] = Fy[0, :, :, 0] + sigamp[np.newaxis, :] # (nM, nTrl, nEp, nY) [nEp x nTrl]
-    
-    # make variable length trials,  and per-trial offset
-    nEp = np.zeros((Fy.shape[-3]), dtype=int)
-    for ti in range(Fy.shape[-3]):
-        # trial offset
-        mu = np.random.standard_normal()
-        Fy[:,  ti, :, :] = Fy[:, ti, :, :] + mu
-        # trial end time
-        nEp[ti] = Fy.shape[-2]*(np.random.uniform()*trlenfrac + (1-trlenfrac))
-        Fy[:, ti, nEp[ti]:, :] = 0
-
-    # no signal at the start of the trial
-    startupNoise_samp = int(Fy.shape[1]*startupNoisefrac)
-    sigamp[:startupNoise_samp] = 0
-    noise[:,:,:startupNoise_samp,:]=0
 
     return Fy, nEp
 

mindaffectBCI/decoder/zscore2Ptgt_softmax.py
@@ -16,6 +16,7 @@
 # along with pymindaffectBCI.  If not, see <http://www.gnu.org/licenses/>
 
 from mindaffectBCI.decoder.normalizeOutputScores import mktestFy
+from mindaffectBCI.decoder.utils import block_permute
 import numpy as np
 
 def zscore2Ptgt_softmax(f, softmaxscale:float=2, prior:np.ndarray=None, validTgt=None, marginalizemodels:bool=True, marginalizedecis:bool=False, peroutputmodel:bool=False):
@@ -155,29 +156,10 @@ def marginalize_scores(f, axis, prior=None, keepdims=False):
 
     return f
 
-def append_block_perm_f(f,n):
-    import random
-    if n < 0 : # neg is max number outputs, so pad with enough
-        n = max(0, -n - f.shape[-1])
-    tmp = f
-    if n > 0 :
-        # use block permutation to make virtual outputs
-        nblk = min( f.shape[-2]/3, 10 )
-        blkSz = int(f.shape[-2]/nblk)
-        # get slices for the blocks
-        blkIdx = [ slice(i,min(f.shape[-2],i+blkSz)) for i in range(0,f.shape[-2],blkSz) ]
-        tmp = np.zeros(f.shape[:-1]+(n+f.shape[-1],), dtype=f.dtype)
-        tmp[...,:f.shape[-1]] = f
-        perm = [ i%f.shape[-1] for i in range(n) ]
-        for idx in blkIdx:
-            random.shuffle(perm) 
-            tmp[...,idx,f.shape[-1]:] = f[...,idx,perm]
-    return tmp
-
 def calibrate_softmaxscale(f, validTgt=None, 
                            scales=(.01,.02,.05,.1,.2,.3,.4,.5,1,1.5,2,2.5,3,3.5,4,5,7,10,15,20,30,50,100), 
-                           MINP=.01, marginalizemodels=True, marginalizedecis=False, eta=.05, 
-                           nocontrol_condn=.5, n_virt_outputs=-30):
+                           MINP=.01, marginalizemodels=True, marginalizedecis=False, 
+                           nocontrol_condn=0, n_virt_outputs=-15):
     '''
     attempt to calibrate the scale for a softmax decoder to return calibrated probabilities
 
@@ -203,7 +185,10 @@ def calibrate_softmaxscale(f, validTgt=None,
         vtgt_nc = np.any(f_nc != 0, axis=(-4,-2) if f.ndim>3 else -2) # (nTrl,nY)
 
     if n_virt_outputs is not None:
-        f = append_block_perm_f(f,n_virt_outputs)
+        # generate virtual outputs for testing -- not from the 'true' target though
+        virt_f = block_permute(f[...,1:], n_virt_outputs, axis=-1, perm_axis=-2)
+        print("Added {} virt outputs\n".format(virt_f.shape[-1]))
+        f = np.append(f,virt_f,axis=-1)
 
     validTgt = np.any(f != 0, axis=(-4,-2) if f.ndim>3 else -2) # (nTrl,nY)
 